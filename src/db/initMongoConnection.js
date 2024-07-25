a8573379270b0facef0a622e181efb47a8dc23e8
RadchenkoArtem2023 <143721793+RadchenkoArtem2023@users.noreply.github.com>
2024-07-25 15:21:27 +0300

    hw_2_task_ver1.1

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose');
 
-const initMongoConnection = async () => {
+async function initMongoConnection() {
+  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } =
+    process.env;
+
+  const connectionString = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+
   try {
-    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
-    await mongoose.connect(uri);
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('Mongo connection successfully established!');
   } catch (error) {
-    console.error('Mongo connection error:', error);
-    throw error;
+    console.error('Mongo connection failed:', error.message);
+    process.exit(1);
   }
-};
+}
 
 module.exports = initMongoConnection;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ require('dotenv').config();
 const setupServer = require('./server');
 const initMongoConnection = require('./db/initMongoConnection');
 
-const startServer = async () => {
-  try {
-    await initMongoConnection();
-    setupServer();
-  } catch (error) {
-    console.error('Failed to start server:', error);
-  }
-};
-
-startServer();
+(async () => {
+  await initMongoConnection();
+  setupServer();
+})();
