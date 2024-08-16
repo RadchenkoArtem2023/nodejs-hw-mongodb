9b2cf0267405a9dac8e85826f3a80984b3e3655c
RadchenkoArtem2023 <143721793+RadchenkoArtem2023@users.noreply.github.com>
2024-08-16 10:27:57 +0300

    fix_task2

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,15 +2,19 @@ const Joi = require('joi');
 
 const contactSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+  phoneNumber: Joi.string().required(),
+  email: Joi.string().email().optional(),
+  isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('work', 'home', 'personal').optional(),
 });
 
 const updateContactSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string().email(),
-  phone: Joi.string(),
-}).or('name', 'email', 'phone');
+  name: Joi.string().optional(),
+  phoneNumber: Joi.string().optional(),
+  email: Joi.string().email().optional(),
+  isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('work', 'home', 'personal').optional(),
+}).or('name', 'phoneNumber', 'email', 'isFavourite', 'contactType');
 
 module.exports = {
   contactSchema,
