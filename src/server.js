9dbb2421b58e3e03f46f206ae533b40383b334fe
RadchenkoArtem2023 <143721793+RadchenkoArtem2023@users.noreply.github.com>
2024-08-30 14:41:45 +0300

    hw_7_task_ver1

--- src/server.js
+++ src/server.js
@@ -11,6 +11,11 @@ const app = express();
 
 const cookieParser = require('cookie-parser');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../docs/swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(cookieParser());
 
 app.use(morgan('dev'));
